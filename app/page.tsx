eb4231d83a6fd70848c21b186b50d94647dcfb05
Potpot13 <talitejohnpaul@gmail.com>
2024-08-07 16:58:04 +0800

    test test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello I'm under the water.</h1>
+      <h1>Hello Im under the water</h1>
     </main>
   );
 }
